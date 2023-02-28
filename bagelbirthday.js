de86c6b859f60a68db96a99e293a43123161dda8
elijor <ejg96@drexel.edu>
2023-02-27 20:37:22 -0500

    added basic sfx

--- bagelbirthday.js
+++ bagelbirthday.js
@@ -1,47 +1,36 @@
-function Bagel(name, description, image) {
+function Bagel(name, description, image, sound) {
     this.name = name;
     this.description = description;
     this.image = image;
+    this.sound = sound;
+
+    const soundpath =  "sfx/";
+    const soundtype = ".wav";
+    this.fullsound = `${soundpath}${Bagel.sound}${soundtype}`;
 
     const path= "imgs/large_bagel_imgs/";
     const imgtype = ".png";
     this.largeimgfull = `${path}${this.image}${imgtype}`;
+
     giveMeName = function() {
-        return this.name;
+        console.log(this.fullsound) ;
     };
 };
 
-
-
-
-/*
-const blueberryBagel = new Bagel("Blueberry Bagel", "You’re incredibly curious about the world around you and like to question everything. You tend to favor what’s new and exciting, but you’re also willing to reflect on classics.", "Large-Blueberry");
-const cheeseBagel = new Bagel("Cheese Bagel", "You have a lot of energy, but when you put it to good use great things can happen. You’re usually the first to say yes to an opportunity and have the most fun in any situation.", "Large-Cheese");
-const cinnamonBagel = new Bagel("Cinnamon Bagel", "You are an old soul and have gone through many experiences that you’ve learned so much from. While you retain an aura of maturity, you’re not afraid to do new things.", 2);
-const eggBagel = new Bagel("Egg Bagel", "You are in tune with your emotions and love to listen to others. Your kindness attracts others to you for guidance, friendship, or just to connect.", 3);
-const everythingBagel = new Bagel("Everything Bagel", "You’re well known and well liked. You manage to be in the spotlight everywhere you go. You have many different talents and skills and can be considered a jack of all trades.", 4 );
-const frToastBagel = new Bagel("French Toast Bagel", "You put others first and love to give a helping hand. Sometimes you give too much and need to find time for yourself to recharge and relax.", 5);
-const garlicBagel = new Bagel("Garlic Bagel", "You put others first and love to give a helping hand. Sometimes you give too much and need to find time for yourself to recharge and relax", 6);
-const onionBagel = new Bagel("Onion Bagel", "You find it easy to adapt to a variety of situations and love to explore new ideas and concepts. Your willingness to change and go with the flow helps you succeed in numerous ways.", 7 );
-const poppySeedBagel = new Bagel("Poppy Seed Bagel", "You like to see things through a positive perspective and favor looking on the brighter side. You are great at solving conflicts because of this and keep a optimistic attitude towards life.", 8);
-const rainbowBagel = new Bagel("Rainbow Bagel", "You’re incredibly innovative and creative. You love to express yourself in various ways and create. Others see your artistic style inside and out and want to learn more.", 9);
-const sesameSeedBagel = new Bagel("Sesame Seed Bagel", "You’re a pretty straightforward person that enjoys the simple things in life. You have a lot of ideas and great things to share with others in your life.", 10);
-const saltBagel = new Bagel("Salt Bagel", "You have high ambitions and are a go getter. You are more material than you’d like to admit, but never gets in the way of accomplishing your goals.", 11);
-*/
 let bagels = [];
 
-bagels.push(new Bagel("Blueberry Bagel", "You're incredibly curious about the world around you and like to question everything. You tend to favor what's new and exciting, but you're also willing to reflect on classics.", "Large-Blueberry"));
-bagels.push(new Bagel("Cheese Bagel", "You have a lot of energy, but when you put it to good use great things can happen. You're usually the first to say yes to an opportunity and have the most fun in any situation.", "Large-Cheese"));
-bagels.push(new Bagel("Cinnamon Bagel", "You are an old soul and have gone through many experiences that you've learned so much from. While you retain an aura of maturity, you're not afraid to do new things.", "Large-Cinnamon"));
-bagels.push(new Bagel("Egg Bagel", "You are in tune with your emotions and love to listen to others. Your kindness attracts others to you for guidance, friendship, or just to connect.", "Large-Egg"));
-bagels.push(new Bagel("Everything Bagel", "You're well known and well liked. You manage to be in the spotlight everywhere you go. You have many different talents and skills and can be considered a jack of all trades.", "Large-Everything" ));
-bagels.push(new Bagel("French Toast Bagel", "You put others first and love to give a helping hand. Sometimes you give too much and need to find time for yourself to recharge and relax.", "Large-FrToast"));
-bagels.push(new Bagel("Garlic Bagel", "You add spice to life with your hilarious sense of humor and unbelievable stories. You like to share your experiences and jokes with the people around you and you're always open to making new friends", "Large-Garlic"));
-bagels.push(new Bagel("Onion Bagel", "You find it easy to adapt to a variety of situations and love to explore new ideas and concepts. Your willingness to change and go with the flow helps you succeed in numerous ways.", "Large-Onion"));
-bagels.push(new Bagel("Poppy Seed Bagel", "You like to see things through a positive perspective and favor looking on the brighter side. You are great at solving conflicts because of this and keep a optimistic attitude towards life.", "Large-Poppy"));
-bagels.push(new Bagel("Rainbow Bagel", "You're incredibly innovative and creative. You love to express yourself in various ways and create. Others see your artistic style inside and out and want to learn more.", "Large-Rainbow"));
-bagels.push(new Bagel("Sesame Seed Bagel", "You're a pretty straightforward person that enjoys the simple things in life. You have a lot of ideas and great things to share with others in your life.", "Large-Sesame"));
-bagels.push(new Bagel("Salt Bagel", "You have high ambitions and are a go getter. You are more material than you'd like to admit, but never gets in the way of accomplishing your goals.", "Large-Salt"));
+bagels.push(new Bagel("Blueberry Bagel", "You're incredibly curious about the world around you and like to question everything. You tend to favor what's new and exciting, but you're also willing to reflect on classics.", "Large-Blueberry", 1));
+bagels.push(new Bagel("Cheese Bagel", "You have a lot of energy, but when you put it to good use great things can happen. You're usually the first to say yes to an opportunity and have the most fun in any situation.", "Large-Cheese", 2));
+bagels.push(new Bagel("Cinnamon Bagel", "You are an old soul and have gone through many experiences that you've learned so much from. While you retain an aura of maturity, you're not afraid to do new things.", "Large-Cinnamon", 3));
+bagels.push(new Bagel("Egg Bagel", "You are in tune with your emotions and love to listen to others. Your kindness attracts others to you for guidance, friendship, or just to connect.", "Large-Egg", 4));
+bagels.push(new Bagel("Everything Bagel", "You're well known and well liked. You manage to be in the spotlight everywhere you go. You have many different talents and skills and can be considered a jack of all trades.", "Large-Everything",5 ));
+bagels.push(new Bagel("French Toast Bagel", "You put others first and love to give a helping hand. Sometimes you give too much and need to find time for yourself to recharge and relax.", "Large-FrToast", 6));
+bagels.push(new Bagel("Garlic Bagel", "You add spice to life with your hilarious sense of humor and unbelievable stories. You like to share your experiences and jokes with the people around you and you're always open to making new friends", "Large-Garlic", 7));
+bagels.push(new Bagel("Onion Bagel", "You find it easy to adapt to a variety of situations and love to explore new ideas and concepts. Your willingness to change and go with the flow helps you succeed in numerous ways.", "Large-Onion",8));
+bagels.push(new Bagel("Poppy Seed Bagel", "You like to see things through a positive perspective and favor looking on the brighter side. You are great at solving conflicts because of this and keep a optimistic attitude towards life.", "Large-Poppy", 9));
+bagels.push(new Bagel("Rainbow Bagel", "You're incredibly innovative and creative. You love to express yourself in various ways and create. Others see your artistic style inside and out and want to learn more.", "Large-Rainbow", 10));
+bagels.push(new Bagel("Sesame Seed Bagel", "You're a pretty straightforward person that enjoys the simple things in life. You have a lot of ideas and great things to share with others in your life.", "Large-Sesame", 11));
+bagels.push(new Bagel("Salt Bagel", "You have high ambitions and are a go getter. You are more material than you'd like to admit, but never gets in the way of accomplishing your goals.", "Large-Salt", 12));
 
 function clickedBagel(e){
     bagelNum = e.id;
@@ -53,7 +42,8 @@ function clickedBagel(e){
             document.getElementsByClassName("bagelgroup").item(x).style.filter = "grayscale(100%)";
         else
             document.getElementsByClassName("bagelgroup").item(x).style.filter = "grayscale(0%)";
-    }
+    };
+    playsound();
 };
 
 function birthdayselect(e) {
@@ -97,4 +87,14 @@ function birthdayselect(e) {
     }
 };
 
+function playsound() {
+  const sound = bagelNum;
+  const soundpath =  "sfx/";
+  const soundtype = ".wav";
+  const fullsound = `${soundpath}${sound}${soundtype}`;
+  console.log(fullsound);
 
+  const audio = new Audio(fullsound);
+  audio.play();
+
+};
